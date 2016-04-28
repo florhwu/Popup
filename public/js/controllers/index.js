b83ba311ecceec4eb5974482c5b6acad65176871
Arthelon <hsing.daniel@gmail.com>
2016-04-28 22:35:38 +0800

    Implemented canvas mouse listener that draws random image on click

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,24 +1,31 @@
 angular.module('controllers.index', [])
-    .controller('IndexCtrl', function() {
+    .controller('IndexCtrl', function ($log) {
         var vm = this
-        vm.images = []
-        for (var i = 1; i <= 20; i++) {
-           var name = i.toString()+'.jpg'
-           for (var i = name.length; i < 3; i++) {
-               name = '0'+name
-           }
-           vm.images.push(name)
-        }
+
     })
-    .directive('drawing', function($log) {
+    .directive('drawing', function ($log) {
         return {
-            link: function(scope, element) {
+            controller: 'DrawingCtrl',
+            link: function (scope, element) {
                 var ctx = element[0].getContext('2d')
-                element.on('mousedown', function(event) {
+                element.on('mousedown', function (event) {
+                    var image = scope.images[Math.floor(Math.random()*20)]
                     
-                    ctx.pus
+                    ctx.drawImage(image, event.offsetX-25, event.offsetY-25, 50, 50)
                 })
             }
         }
     })
-    .service('')
+    .controller('DrawingCtrl', function ($scope, $document, $log) {
+        $scope.images = []
+        for (var i = 1; i <= 20; i++) {
+            var name = i.toString()
+            for (var x = name.length; x < 3; x++) {
+                name = '0' + name
+            }
+            name += '.jpg'
+            var image = new Image()
+            image.src = '/img/'+name
+            $scope.images.push(image)
+        }
+    })
