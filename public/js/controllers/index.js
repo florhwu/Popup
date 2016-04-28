b0503640699532df629c96b06005e9fc89315d55
Arthelon <hsing.daniel@gmail.com>
2016-04-28 15:23:21 +0800

    Created list for storing image locs

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,14 +1,21 @@
 angular.module('controllers.index', [])
     .controller('IndexCtrl', function() {
         var vm = this
-        this.message = 'Hello World'
+        vm.images = []
+        for (var i = 1; i <= 20; i++) {
+           var name = i.toString()+'.jpg'
+           for (var i = name.length; i < 3; i++) {
+               name = '0'+name
+           }
+           vm.images.push(name)
+        }
     })
     .directive('drawing', function($log) {
         return {
             link: function(scope, element) {
                 var ctx = element[0].getContext('2d')
                 element.on('mousedown', function(event) {
-                    $log.log(event.clientX, event.clientY)
+                    
                 })
             }
         }
