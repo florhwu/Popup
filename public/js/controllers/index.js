f3299b42f3a2b9a4b1b804322eb587fc5373c3e9
Arthelon <hsing.daniel@gmail.com>
2016-04-29 10:14:59 +0800

    Added background beethoven track

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,22 +1,31 @@
 angular.module('controllers.index', [])
-    .controller('IndexCtrl', function ($log) {
+    .controller('IndexCtrl', function () {
         var vm = this
 
     })
-    .directive('drawing', function ($log) {
+    .directive('drawing', function () {
         return {
             controller: 'DrawingCtrl',
             link: function (scope, element) {
                 var ctx = element[0].getContext('2d')
                 element.on('mousedown', function (event) {
                     var image = scope.images[Math.floor(Math.random()*20)]
-                    
-                    ctx.drawImage(image, event.offsetX-25, event.offsetY-25, 50, 50)
+                    var width = (Math.random()*60) + 40
+                    var height = (Math.random()*60) + 40
+                    ctx.drawImage(image, event.offsetX-width/2, event.offsetY-height/2, width, height)
                 })
             }
         }
     })
-    .controller('DrawingCtrl', function ($scope, $document, $log) {
+    .controller('DrawingCtrl', function ($scope) {
+        var audio = new Howl({
+            urls: ['/beethoven.ogg'],
+            autoplay: true,
+            loop: true,
+            buffer: true,
+            rate:10.0
+        })
+        audio.play()
         $scope.images = []
         for (var i = 1; i <= 20; i++) {
             var name = i.toString()
