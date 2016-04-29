5ff34237b107b47f32bd1d629f35d02bafce3f5d
oaklen <florhwu@gmail.com>
2016-04-29 12:32:00 +0800

    header css done

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,7 +1,6 @@
 angular.module('controllers.index', [])
     .controller('IndexCtrl', function () {
         var vm = this
-
     })
     .directive('drawing', function () {
         return {
@@ -10,8 +9,8 @@ angular.module('controllers.index', [])
                 var ctx = element[0].getContext('2d')
                 element.on('mousedown', function (event) {
                     var image = scope.images[Math.floor(Math.random()*20)]
-                    var width = (Math.random()*60) + 40
-                    var height = (Math.random()*60) + 40
+                    var width = (Math.random()*200) + 50
+                    var height = (Math.random()*200) + 50
                     ctx.drawImage(image, event.offsetX-width/2, event.offsetY-height/2, width, height)
                 })
             }
